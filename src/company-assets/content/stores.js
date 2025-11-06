24709d0ea79a3f0432bc5ad69cb0c28e1790e311
sanjayibo <sanjay.kumar@ibo.com>
2025-11-06 12:36:41 +0530

    [Sanjay] Updated Icons

--- src/company-assets/content/stores.js
+++ src/company-assets/content/stores.js
@@ -1,8 +1,9 @@
+import storeLocationsImage from '../assets/Ibo-store.png';
 const storesContent = {
   title: 'Shop ERVO Products at your nearest IBO Stores',
   buttonText: 'Shop Now',
   locations: ['Bengaluru', 'Chennai', 'Hyderabad', 'Pune', 'Coimbatore', 'Mysore'],
-  backgroundImage: 'https://images.unsplash.com/photo-1565843708714-8e6a3ccb6fe4?w=1920&q=80',
+  backgroundImage: storeLocationsImage,
 };
 
 export default storesContent;
