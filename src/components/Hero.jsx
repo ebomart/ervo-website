cc36d35925f295ffdcc441da56bfd6fad0a1c79a
sanjayibo <sanjay.kumar@ibo.com>
2026-02-11 17:09:16 +0530

    [Sanjay] Updated hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,44 @@
 import { heroContent } from "../company-assets/content";
-import arrowRightIcon from "../company-assets/assets/right-circle-outlined.png";
+import downArrowIcon from "../company-assets/assets/down-arrow.png";
 
 const Hero = ({ onExploreClick }) => {
   return (
-    <section
-      className=" mx-auto w-[95%] h-[700px] rounded-3xl flex items-start justify-center bg-cover bg-center"
-      style={{
-        backgroundImage: `url('${heroContent.backgroundImage}')`,
-      }}
-    >
-      <div className=" z-10 text-center px-6 max-w-[100%] pt-20">
-        <h2 className="text-5xl font-bold text-ervo-brown mb-6 leading-tight tracking-tight">
-          {heroContent.title}
-        </h2>
-        <p className="text-lg md:text-xl text-text-gray mb-2 font-normal leading-relaxed">
-          {heroContent.subtitle1}
-        </p>
-        <p className="text-lg md:text-xl text-text-gray mb-10 font-normal leading-relaxed">
-          {heroContent.subtitle2}
-        </p>
-        <button
-          onClick={onExploreClick}
-          className="bg-ervo-brown text-white px-10 py-4 rounded-full text-base font-medium hover:bg-ervo-dark-brown transition-colors flex items-center gap-3 mx-auto shadow-lg cursor-pointer"
-        >
-          {heroContent.buttonText}
-          <img src={arrowRightIcon} alt="arrow right" className="h-5 w-5" />
-        </button>
+    <>
+      <section
+        className="relative mx-auto mb-10 flex h-[600px] w-[95%] items-start justify-center rounded-3xl bg-cover bg-center"
+        style={{
+          background: `
+            linear-gradient(180deg, #000000 0%, #00000000 100%) 0% 0% no-repeat padding-box,
+            url('${heroContent.backgroundImage}') center bottom / cover no-repeat
+          `,
+        }}
+      >
+        <div className="z-10 max-w-full px-6 pt-20 text-center">
+          <h2 className="mb-6 text-5xl leading-tight font-bold tracking-tight text-white">
+            {heroContent.title}
+          </h2>
+          <p className="mb-2 text-lg leading-relaxed font-normal text-white md:text-xl">
+            {heroContent.subtitle1}
+          </p>
+          <p className="mb-10 text-lg leading-relaxed font-normal text-white md:text-xl">
+            {heroContent.subtitle2}
+          </p>
+        </div>
+      </section>
+      <div className="flex items-center justify-center text-[#4B174B] text-[14px] font-semibold pb-2">
+        <span className="text-[14px] font-[600]">{heroContent.buttonText}</span>
       </div>
-    </section>
+      <button
+        onClick={onExploreClick}
+        className="border-[#F6C6F6] border mx-auto flex cursor-pointer items-center gap-3 rounded-full p-2 text-base font-medium text-white shadow-lg transition-colors"
+        style={{
+          background:
+            "radial-gradient(closest-side at 50% 87%, rgba(248,117,248,0.25) 0%, rgba(247,227,255,0.25) 100%) 0% 0% no-repeat padding-box",
+        }}
+      >
+        <img src={downArrowIcon} alt="down arrow" className="h-5 w-5" />
+      </button>
+    </>
   );
 };
 
