825480e61b206248b8f823c6dc72f1f7d6419fb9
sanjayibo <sanjay.kumar@ibo.com>
2026-02-18 11:06:57 +0530

    [Sanjay] Added products

--- src/company-assets/content/products.js
+++ src/company-assets/content/products.js
@@ -1,12 +1,15 @@
 const productsContent = {
   title: "Style Your Space with Confidence",
-  description:"BELZA isn't just a brand - it's your sanctuary of innovation, ease, and elevated everyday living. From intuitive fixtures & fittings to statement-making interior elements, everything you need to create your dream space lives under one smart, sophisticated roof.",
+  description:
+    "BELZA isn't just a brand - it's your sanctuary of innovation, ease, and elevated everyday living. From intuitive fixtures & fittings to statement-making interior elements, everything you need to create your dream space lives under one smart, sophisticated roof.",
   categories: [
     "All Products",
-    "Storage Geyser",
-    "Electric Kettle",
-    "Chopper",
-    "Iron Box",
+    "Angle Valve",
+    "Bib Tap",
+    "Sink Tap",
+    "Pillar Tap",
+    "Wall Mixer",
+    "Waste Coupling",
   ],
   messages: {
     noCategory: "Please select a category to view products.",
@@ -14,48 +17,301 @@ const productsContent = {
   },
   products: [
     {
-      id: 1,
-      name: "Ervo Onsen Vertical Storage Geyser 15L",
-      // type: "Toaster",
-      price: "5,499",
-      // description: "Auto Heat Cutout &#x2022; Auto Heat Cutout",
+      id: "1018787075",
+      name: "BELZA PRJ ANGLE VALVE",
+      price: "349",
       image:
-        "https://services.ibo.com/media/v1/products/images/fc01d29c-3e76-4527-aa57-933f0fc37bf1/ervo-onsen-vertical-storage-geyser-15l-1.webp",
-      link: "https://www.ibo.com/EZI5WGGBN1/p/EZI5WGGBN1",
-      category: "Storage Geyser",
+        "https://services.ibo.com/media/v1/products/images/2754af72-d981-40bc-ad34-c0f1e32c534b/belza-prj-angle-valve-1.webp",
+      link: "https://www.ibo.com/belza-1-way-angle-valve/p/EK2PE8UIRN",
+      category: "Angle Valve",
     },
     {
-      id: 2,
-      name: "Ervo 1.5L Electric Kettle",
-      // type: "Electric Kettle",
-      price: "399",
+      id: "1018787053",
+      name: "BELZA FLUX ANGLE VALVE",
+      price: "549",
       image:
-        "https://services.ibo.com/media/v1/products/images/e74d138b-2490-4904-9f45-89f744fb292b/ervo-15l-electric-kettle-1.webp",
-      // description: "Auto Heat Cutout &#x2022; Auto Heat Cutout",
-      link: "https://www.ibo.com/E75BWAHKUY/p/E75BWAHKUY",
-      category: "Electric Kettle",
+        "https://services.ibo.com/media/v1/products/images/5f1e7f4f-83e9-4424-ae46-04f5ed34c80d/belza-flux-angle-valve-1.webp",
+      link: "https://www.ibo.com/belza-flux-1-way-angle-valve/p/EZLFNPHY4C",
+      category: "Angle Valve",
     },
     {
-      id: 3,
-      name: "Ervo Dry Iron White 1000 W",
-      // type: "E-Kettle",
-      price: "399",
+      id: "1018787031",
+      name: "BELZA AQUA 2 WAY ANGLE VALVE",
+      price: "949",
       image:
-        "https://services.ibo.com/media/v1/products/images/56b0bde8-9209-4d02-9310-862bd2bd1167/ervo-dry-iron-white-1000-w-1.webp",
-      // description: "Auto Heat Cutout &#x2022; Auto Heat Cutout",
-      link: "https://www.ibo.com/EYXFLVBQN5/p/EYXFLVBQN5",
-      category: "Iron Box",
+        "https://services.ibo.com/media/v1/products/images/c2e6a3b8-6a18-426c-a733-fab1b81add9c/belza-aqua-2-way-angle-valve-1.webp",
+      link: "https://www.ibo.com/belza-aqua-2-way-angle-valve/p/EEGSFIB3WT",
+      category: "Angle Valve",
     },
     {
-      id: 4,
-      name: "Ervo Handy 3 Blades Chopper 400ML",
-      // type: "Illumination",
-      price: "4,989",
-      link: "https://www.ibo.com/ER9ONOMPUC/p/ER9ONOMPUC",
+      id: "1018787018",
+      name: "BELZA AQUA ANGLE VALVE",
+      price: "549",
       image:
-        "https://services.ibo.com/media/v1/products/images/b0269799-1782-43a0-bc9e-abc5064ca84e/ervo-handy-3-blades-chopper-400ml-1.webp",
-      category: "Chopper",
-      // description: "Auto Heat Cutout &#x2022; Auto Heat Cutout",
+        "https://services.ibo.com/media/v1/products/images/1ee9efe8-e0ba-4756-ba9e-6a0cf7e65a31/belza-aqua-angle-valve-1.webp",
+      link: "https://www.ibo.com/belza-aqua-1-way-angle-valve/p/EPJWVTC6C1",
+      category: "Angle Valve",
+    },
+    {
+      id: "1018786996",
+      name: "BELZA NOVA 2 WAY ANGLE VALVE",
+      price: "1,049",
+      image:
+        "https://services.ibo.com/media/v1/products/images/5a5f8551-d03b-43c2-8e66-41fb99057cd3/belza-nova-2-way-angle-valve-1.webp",
+      link: "https://www.ibo.com/belza-nova-2-way-angle-valve/p/EFRNBRSD1P",
+      category: "Angle Valve",
+    },
+    {
+      id: "1018786974",
+      name: "BELZA NOVA ANGLE VALVE",
+      price: "649",
+      image:
+        "https://services.ibo.com/media/v1/products/images/a78bbc9d-18be-49d9-9bee-0545fa7e3e46/belza-nova-angle-valve-1.webp",
+      link: "https://www.ibo.com/belza-nova-1-way-angle-valve/p/ETZR39GMAR",
+      category: "Angle Valve",
+    },
+    {
+      id: "1018787731",
+      name: "BELZA AQUA 2 WAY BIB TAP - Double lever",
+      price: "999",
+      image:
+        "https://services.ibo.com/media/v1/products/images/008477ef-9a48-4d26-b7e8-f1017b224e16/belza-aqua-2-way-bib-tap--double-lever-1.webp",
+      link: "https://www.ibo.com/belza-aqua-dual-lever-bib-tap/p/ESXHD67Y30",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787307",
+      name: "BELZA GARDEN NOZZLE BIB TAP",
+      price: "649",
+      image:
+        "https://services.ibo.com/media/v1/products/images/046a14ec-548f-46de-bea3-c3bd25ef8938/belza-garden-nozzle-bib-tap-1.webp",
+      link: "https://www.ibo.com/belza-single-lever-bib-tap/p/E25GDPDFBU",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787286",
+      name: "BELZA WM NOZZLE BIB TAP",
+      price: "649",
+      image:
+        "https://services.ibo.com/media/v1/products/images/5df3dc50-66c8-4c9e-98d8-61668ec047cd/belza-wm-nozzle-bib-tap-1.webp",
+      link: "https://www.ibo.com/belza-single-lever-bib-tap/p/EKTMNJPXDC",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787264",
+      name: "BELZA FLUX BIB TAP",
+      price: "649",
+      image:
+        "https://services.ibo.com/media/v1/products/images/54746d9c-65b0-402b-a70b-6c0e2d9d938d/belza-flux-bib-tap-1.webp",
+      link: "https://www.ibo.com/belza-flux-single-lever-bib-tap/p/ENU4KAIJQC",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787242",
+      name: "BELZA STREAM LONG BODY BIB TAP",
+      price: "749",
+      image:
+        "https://services.ibo.com/media/v1/products/images/811f4db7-ae14-44e0-b360-4d03ccd45528/belza-stream-long-body-bib-tap-1.webp",
+      link: "https://www.ibo.com/belza-stream-single-lever-bib-tap/p/EQ5TELYIEH",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787229",
+      name: "BELZA STREAM BIB TAP",
+      price: "649",
+      image:
+        "https://services.ibo.com/media/v1/products/images/0da6e8d6-7791-4ab0-a8c0-4e868a89979f/belza-stream-bib-tap-1.webp",
+      link: "https://www.ibo.com/belza-stream-single-lever-bib-tap/p/E5MW6SFZAR",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787197",
+      name: "BELZA AQUA LONG BODY BIB TAP",
+      price: "749",
+      image:
+        "https://services.ibo.com/media/v1/products/images/aad99b1c-fcfe-41db-8212-d6b75c71b1b9/belza-aqua-long-body-bib-tap-1.webp",
+      link: "https://www.ibo.com/belza-aqua-single-lever-bib-tap/p/EQSA5QZDN4",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787153",
+      name: "BELZA NOVA 2 WAY BIB TAP-Single lever",
+      price: "1,149",
+      image:
+        "https://services.ibo.com/media/v1/products/images/018e0597-b1c9-4ed9-82a8-ab7b9bc7d01d/belza-nova-2-way-bib-tapsingle-lever-1.webp",
+      link: "https://www.ibo.com/belza-nova-single-lever-bib-tap/p/EGSEZQE819",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787131",
+      name: "BELZA NOVA LONG BODY BIB TAP",
+      price: "899",
+      image:
+        "https://services.ibo.com/media/v1/products/images/5b885118-93ab-422f-a5c3-a7bc90019599/belza-nova-long-body-bib-tap-1.webp",
+      link: "https://www.ibo.com/belza-nova-single-lever-bib-tap/p/EDOSUALCCN",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787118",
+      name: "BELZA NOVA BIB TAP",
+      price: "799",
+      image:
+        "https://services.ibo.com/media/v1/products/images/372afdde-0cb5-43a2-9778-ddad674cd1a5/belza-nova-bib-tap-1.webp",
+      link: "https://www.ibo.com/belza-nova-single-lever-bib-tap/p/EY5JUH2CIW",
+      category: "Bib Tap",
+    },
+    {
+      id: "1018787464",
+      name: "BELZA FLUX SINK TAP",
+      price: "1,049",
+      image:
+        "https://services.ibo.com/media/v1/products/images/5c500f6d-3479-4f06-bb5f-435a70cf179f/belza-flux-sink-tap-1.webp",
+      link: "https://www.ibo.com/belza-flux-wall-mounted-silver-sink-tap/p/EQPULAY78S",
+      category: "Sink Tap",
+    },
+    {
+      id: "1018787442",
+      name: "BELZA STREAM SINK TAP FLEXI- Grey",
+      price: "1,199",
+      image:
+        "https://services.ibo.com/media/v1/products/images/1b3c44fb-46b9-4434-bc12-5261b009718f/belza-stream-sink-tap-flexi-grey-1.webp",
+      link: "https://www.ibo.com/belza-stream-wall-mounted-grey-sink-tap/p/EKJY2PHL3X",
+      category: "Sink Tap",
+    },
+    {
+      id: "1018787429",
+      name: "BELZA STREAM SINK TAP FLEXI- Black",
+      price: "1,199",
+      image:
+        "https://services.ibo.com/media/v1/products/images/a9fb7258-4961-497a-9b54-145e399554ec/belza-stream-sink-tap-flexi-black-1.webp",
+      link: "https://www.ibo.com/belza-stream-wall-mounted-black-sink-tap/p/E8YTMQVCFO",
+      category: "Sink Tap",
+    },
+    {
+      id: "1018787386",
+      name: "BELZA AQUA SINK TAP FLEXI- Grey",
+      price: "1,199",
+      image:
+        "https://services.ibo.com/media/v1/products/images/0b392154-1175-411e-ac4e-4dbfc7342334/belza-aqua-sink-tap-flexi-grey-1.webp",
+      link: "https://www.ibo.com/belza-aqua-wall-mounted-grey-sink-tap/p/EIXFJPZ80A",
+      category: "Sink Tap",
+    },
+    {
+      id: "1018787364",
+      name: "BELZA AQUA SINK TAP FLEXI - Black",
+      price: "1,199",
+      image:
+        "https://services.ibo.com/media/v1/products/images/9a7f4298-d9e4-404d-855e-3ce98e15dec7/belza-aqua-sink-tap-flexi--black-1.webp",
+      link: "https://www.ibo.com/belza-aqua-wall-mounted-black-sink-tap/p/EJZLPN6WE8",
+      category: "Sink Tap",
+    },
+    {
+      id: "1018787342",
+      name: "BELZA AQUA SINK TAP",
+      price: "1,049",
+      image:
+        "https://services.ibo.com/media/v1/products/images/63006007-258c-40a4-b2ff-06665b965340/belza-aqua-sink-tap-1.webp",
+      link: "https://www.ibo.com/belza-aqua-wall-mounted-silver-sink-tap/p/EUAQPKFWH8",
+      category: "Sink Tap",
+    },
+    {
+      id: "1018787329",
+      name: "BELZA NOVA SINK TAP",
+      price: "1,149",
+      image:
+        "https://services.ibo.com/media/v1/products/images/3a426bcd-fe52-4320-b3b0-db6f5fb805cb/belza-nova-sink-tap-1.webp",
+      link: "https://www.ibo.com/belza-nova-wall-mounted-silver-sink-tap/p/E2IZHQPKS3",
+      category: "Sink Tap",
+    },
+    {
+      id: "1018787707",
+      name: "BELZA FLUX PILLAR TAP",
+      price: "749",
+      image:
+        "https://services.ibo.com/media/v1/products/images/93e7aeec-92d1-4a63-8df8-249c71608d5f/belza-flux-pillar-tap-1.webp",
+      link: "https://www.ibo.com/belza-flux-flux-101-chrome-pillar-tap/p/E97YSMXC8A",
+      category: "Pillar Tap",
+    },
+    {
+      id: "1018787686",
+      name: "BELZA STREAM PILLAR TAP",
+      price: "749",
+      image:
+        "https://services.ibo.com/media/v1/products/images/882054d1-6da5-4a89-91e8-2d653c86e8ca/belza-stream-pillar-tap-1.webp",
+      link: "https://www.ibo.com/belza-stream-strm-701-chrome-pillar-tap/p/E8YEWCS0AN",
+      category: "Pillar Tap",
+    },
+    {
+      id: "1018787642",
+      name: "BELZA AQUA PILLAR TAP",
+      price: "749",
+      image:
+        "https://services.ibo.com/media/v1/products/images/99b3872c-6498-4516-811a-63e96eaf2443/belza-aqua-pillar-tap-1.webp",
+      link: "https://www.ibo.com/belza-aqua-aqua-101-chrome-pillar-tap/p/EMCIPHN8XW",
+      category: "Pillar Tap",
+    },
+    {
+      id: "1018787629",
+      name: "BELZA NOVA TALL BODY PILLAR TAP",
+      price: "1,699",
+      image:
+        "https://services.ibo.com/media/v1/products/images/eca32049-b949-4aa0-8059-832aacf5c44a/belza-nova-tall-body-pillar-tap-1.webp",
+      link: "https://www.ibo.com/belza-nova-nova-104-chrome-pillar-tap/p/E2VIRNAK70",
+      category: "Pillar Tap",
+    },
+    {
+      id: "1018787564",
+      name: "BELZA FLUX 2 in 1 WALL MIXER",
+      price: "2,999",
+      image:
+        "https://services.ibo.com/media/v1/products/images/3f6d6a0a-b0d8-4cdc-992e-f3a27c5a4a97/belza-flux-2-in-1-wall-mixer-1.webp",
+      link: "https://www.ibo.com/belza-flux-flux-151-2-in-1-wall-mixer/p/E7HDPTXMHY",
+      category: "Wall Mixer",
+    },
+    {
+      id: "1018787542",
+      name: "BELZA STREAM 2 in 1 WALL MIXER",
+      price: "2,999",
+      image:
+        "https://services.ibo.com/media/v1/products/images/fd5f7ced-668e-452f-891a-dd7affa0f8fe/belza-stream-2-in-1-wall-mixer-1.webp",
+      link: "https://www.ibo.com/belza-stream-strm-722-2-in-1-wall-mixer/p/EZ7AS13X9C",
+      category: "Wall Mixer",
+    },
+    {
+      id: "1018787529",
+      name: "BELZA AQUA 3 IN 1 WALL MIXER",
+      price: "3,499",
+      image:
+        "https://services.ibo.com/media/v1/products/images/c8df9cf1-38bc-4a63-8de9-1ff641e25fc8/belza-aqua-3-in-1-wall-mixer-1.webp",
+      link: "https://www.ibo.com/belza-aqua-aqua-153-3-in-1-wall-mixer/p/E3RUDCZAVI",
+      category: "Wall Mixer",
+    },
+    {
+      id: "1018787507",
+      name: "BELZA AQUA 2 in 1 WALL MIXER",
+      price: "2,999",
+      image:
+        "https://services.ibo.com/media/v1/products/images/9e4fd569-971f-4436-be8e-d82beae0f5fe/belza-aqua-2-in-1-wall-mixer-1.webp",
+      link: "https://www.ibo.com/belza-aqua-aqua-151-2-in-1-wall-mixer/p/EENI0XBD25",
+      category: "Wall Mixer",
+    },
+    {
+      id: "1018787797",
+      name: 'BELZA WC 3" HALF THREAD',
+      price: "299",
+      image:
+        "https://services.ibo.com/media/v1/products/images/96be2fee-aa5a-46bf-bdc2-6a871e4b6b07/belza-wc-3-half-thread-1.webp",
+      link: "https://www.ibo.com/belza-40-mm-3-in-half-threaded-brass-waste-coupling/p/EO8F9ABY1E",
+      category: "Waste Coupling",
+    },
+    {
+      id: "1018787775",
+      name: 'BELZA WC 3" FULL THREAD',
+      price: "299",
+      image:
+        "https://services.ibo.com/media/v1/products/images/4363a53f-eb9b-4ca6-86c8-91f2d659610e/belza-wc-3-full-thread-1.webp",
+      link: "https://www.ibo.com/belza-40-mm-6-in-full-threaded-brass-waste-coupling/p/ECWQKFP57O",
+      category: "Waste Coupling",
     },
   ],
 };
